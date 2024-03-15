fdf3e40cb785f621de67eb83018e57a0437842c4
Rajeshkumar-7 <83906427+Rajeshkumar-7@users.noreply.github.com>
2024-03-15 18:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+
+	arr.sort();
+while(arr.length > 1){
+    let a = arr.shift();
+    let b = arr.shift();
+    arr.push(a + b);
+    arr.sort();
+}
+return arr.pop();
 }
 
 module.exports=mincost;
